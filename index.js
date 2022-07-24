40aec6f3d7a18fc060936bdc91c15ed3309fac27
Rhuan Nascimento Ferreria <rhuan2414@gmail.com>
2022-07-24 15:30:53 -0300

    Implementando funcionalidade de adicioanr inscrito

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const showLists = document.querySelector('#btn');
+const add = document.querySelector('#add');
 const list = document.querySelector('.list');
 list.style.display = "none";
+
+add.addEventListener('click', () =>{
+
+    const name = document.querySelector('#name');
+    const age = document.querySelector('#age');
+    const senior = document.querySelector('#seniorList');
+    const master = document.querySelector('#masterList');
+    const junior = document.querySelector('#juniorList');
+ 
+     const listItem = document.createElement("li");
+ 
+     listItem.textContent = `${name.value}, ${age.value}` 
+ 
+     if(name.value != "" && age.value != ""){
+         if(age.value < 18){
+             junior.appendChild(listItem);
+         }else if(age.value >= 18 && age.value < 40){
+             master.appendChild(listItem);
+         }else{
+             senior.appendChild(listItem);
+         }
+     }else{
+         window.alert('Algum campo está incompleto')
+     }
+ 
+     name.value="";
+     age.value="";
+     name.focus();
+ 
+ } )
+
 
 showLists.addEventListener('click', () =>{
     
